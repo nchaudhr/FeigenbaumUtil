6da241543e60b483a20d972aba88f827de6b2db5
Naveed Iqbal <nchaudhr@gmail.com>
2017-05-15 23:21:41 -0400

    Fresh docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-=deploydocs(
-     repo = "https://github.com/nchaudhr/FeigenbaumUtil"
- )=#
+#=deploydocs(
+    repo = "<repository url>"
+)=#
