e82af869bb040aba0084080adf6f9a511b8a13de
Naveed Iqbal <nchaudhr@gmail.com>
2017-05-17 12:25:28 -0400

    Fix repo name in deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,7 @@ makedocs(
 # for more information.
 deploydocs(
     deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
-    repo = "github.com/nchaudhr/FeigenbaumUtil",
+    repo = "github.com/nchaudhr/FeigenbaumUtil.git",
     julia  = "0.5",
     osname = "linux"
     # ...
