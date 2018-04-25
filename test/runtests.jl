88bb9b49771fd891f4df68f7ebd460a3b2c2d788
Jonathan Goldfarb <jgoldfar@gmail.com>
2018-04-24 22:01:04 -0400

    Test that all files that we can load will continue to load

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,21 @@ else
     using Base.Test
 end
 
-@testset begin
+@testset "FeigenbaumUtil" begin
     include("test_libCore.jl") # test FeigenbaumCore
     include("test_libSecondMinimal.jl") # test FeigenbaumSecondMinimal
+    @testset "ThirdMin_GenerateCases" begin
+        include("ThirdMin_GenerateCases.jl")
+        @test true # example runs
+    end
+    dataDirFiles = filter(s->startswith(s, "SecondMinimal"), readdir(joinpath(dirname(@__DIR__), "data")))
+    @testset "dataFile $d runs" for d in dataDirFiles
+        include(joinpath(dirname(@__DIR__), "data", d))
+        @test true
+    end
+    MakeDigraphsDirFiles = filter(s->startswith(s, "case"), readdir(joinpath(dirname(@__DIR__), "MakeDigraphs")))
+    @testset "makeDigraphs $d runs" for d in MakeDigraphsDirFiles
+        include(joinpath(dirname(@__DIR__), "MakeDigraphs", d))
+        @test true
+    end
 end
